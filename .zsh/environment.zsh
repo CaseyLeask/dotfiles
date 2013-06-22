0127b6874fe422ad729a28b076895d525e52ee22
Casey Leask <caseyleask@gmail.com>
2013-06-22 17:58:52 +1000

    For Postgres.app

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -4,6 +4,7 @@ export PATH="$HOME/bin:/usr/local/Cellar/ruby/1.9.3-p327/bin:$PATH"
 export CC=/usr/bin/gcc-4.2
 export EDITOR=/usr/bin/vim
 export CLICOLOR=1
+export PGHOST="localhost"
 
 # command history
 HISTFILE=~/.zsh_history
